b61af774e6e40879a5f85b251b51a6f3ada95ecf
Billy Babicz <wjbabicz@gmail.com>
2014-05-04 19:18:19 -0500

    Initial commit.

---
+++ controllers/default.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+## - call exposes all registered services (none by default)
+#########################################################################
+
+
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    return dict(message=T('Welcome!'))
+
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+
+    return dict(form=auth())
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+
+@auth.requires_signature()
+def data():
+    """
+    http://..../[app]/default/data/tables
+    http://..../[app]/default/data/create/[table]
+    http://..../[app]/default/data/read/[table]/[id]
+    http://..../[app]/default/data/update/[table]/[id]
+    http://..../[app]/default/data/delete/[table]/[id]
+    http://..../[app]/default/data/select/[table]
+    http://..../[app]/default/data/search/[table]
+    but URLs must be signed, i.e. linked with
+      A('table',_href=URL('data/tables',user_signature=True))
+    or with the signed load operator
+      LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
+    """
+    return dict(form=crud())
+
+
+
+
+
+
+@auth.requires_login()
+def Test():
+    return locals()
+
+@auth.requires_login()
+def profile():
+    return locals()
+
+@auth.requires_login()
+def keyboard():
+    return locals()
+
+#@auth.requires_membership("admin") # uncomment to enable security
+@auth.requires_membership('Administrator')
+def list_users():
+    btn = lambda row: A("Edit", _href=URL('manage_user', args=row.auth_user.id))
+    db.auth_user.edit = Field.Virtual(btn)
+    rows = db(db.auth_user).select()
+    headers = ["ID", "Name", "Last Name", "Email", "Edit"]
+    fields = ['id', 'first_name', 'last_name', "email", "edit"]
+    table = TABLE(THEAD(TR(*[B(header) for header in headers])),
+                  TBODY(*[TR(*[TD(row[field]) for field in fields]) \
+                        for row in rows]))
+    table["_class"] = "table table-striped table-bordered table-condensed"
+    return dict(table=table)
+
+#@auth.requires_membership("admin") # uncomment to enable security
+@auth.requires_membership('Administrator')
+def manage_user():
+    user_id = request.args(0) or redirect(URL('list_users'))
+    form = SQLFORM(db.auth_user, user_id).process()
+    membership_panel = LOAD(request.controller,
+                            'manage_membership.html',
+                             args=[user_id],
+                             ajax=True)
+    return dict(form=form,membership_panel=membership_panel)
+
+def give_editor_poermission(form):
+    group_id = auth.id_group("User")
+    auth.add_membership(group_id,  auth.user.id)
+
+@auth.requires_membership('Administrator')
+def manage_membership():
+    user_id = request.args(0) or redirect(URL('list_users'))
+    db.auth_membership.user_id.default = int(user_id)
+    db.auth_membership.user_id.writable = False
+    form = SQLFORM.grid(db.auth_membership.user_id == user_id,
+                       args=[user_id],
+                       searchable=False,
+                       deletable=False,
+                       details=False,
+                       selectable=False,
+                       csv=False,
+                       user_signature=False)  # change to True in production
+    return form
+
+@auth.requires_membership('Administrator')
+def admin_membership():
+    form=SQLFORM.grid(db.auth_membership)
+
+    return locals()
+@auth.requires_membership('Administrator')
+def admin_user():
+    form=SQLFORM.grid(db.auth_user)
+    return locals()
+
+@auth.requires_membership('Administrator')
+def admin():
+    return locals()
---
+++ views/default/keyboard.html
@@ -0,0 +1,1 @@
+<h1>KEYBOARD</h1>
